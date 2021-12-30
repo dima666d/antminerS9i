8412d2f39b5ce29d1dd5e6bd2b0c2f4748203461
DVD <dima@tevirp.ru>
2021-12-30 15:51:24 +0500

    fix template

--- s9.info.sh
+++ s9.info.sh
@@ -3,6 +3,9 @@
 # Just put "s9*.sh" files to the Zabbix ExternalScripts directory (/etc/zabbix/externalscripts by default).
 # Required utilites: nc, timeout.
 # only bash, no perl, no python and no etc
+# apt install ncat
+# or
+# yum install nmap-ncat
 
 # If you like my work, please consider donating:
 # BTC: 39DyYL4NxvBv1BTR7RK9UJJisqhTCQhFFa
